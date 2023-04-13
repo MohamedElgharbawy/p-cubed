4939f33c0550c99278b057aa289156e8751bd8e3
Mohamed Elgharbawy <mwelgharb@berkeley.edu>
2023-04-12 22:23:57 -0700

    Updated firebase logic

---
+++ public/firebase/config.js
@@ -0,0 +1,17 @@
+// import { initializeApp } from "firebase/app";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD1PjddxLBZKt4-tmH3Txo7EOgr6iKmJUA",
+    authDomain: "p-cubed-ecbe5.firebaseapp.com",
+    projectId: "p-cubed-ecbe5",
+    storageBucket: "p-cubed-ecbe5.appspot.com",
+    messagingSenderId: "536291594426",
+    appId: "1:536291594426:web:4588e19b29e58563d53283",
+    measurementId: "G-ZRGMCG79C0"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export {firebaseApp}
--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js'
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { firebaseApp } from '../firebase/config.js';
 
 function signInGoogle() {
-    const firebaseConfig = {
-        apiKey: "AIzaSyD1PjddxLBZKt4-tmH3Txo7EOgr6iKmJUA",
-        authDomain: "p-cubed-ecbe5.firebaseapp.com",
-        projectId: "p-cubed-ecbe5",
-        storageBucket: "p-cubed-ecbe5.appspot.com",
-        messagingSenderId: "536291594426",
-        appId: "1:536291594426:web:4588e19b29e58563d53283",
-        measurementId: "G-ZRGMCG79C0"
-    };
-
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-
     const provider = new GoogleAuthProvider();
     const auth = getAuth();
     signInWithPopup(auth, provider)
